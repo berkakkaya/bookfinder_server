34f1c2dd7de5046866fdf26fd9c6aa99093bde0c
Berk Akkaya <berkakkaya000@gmail.com>
2024-12-04 22:50:33 +0300

    routes: add endpoint for book recommendations

---
+++ routes/recommendation_algorithm/book_recommendations.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, request, jsonify
+from services.database import db_provider
+from utils.flask_auth import login_required
+
+bp = Blueprint("book_recommendations", __name__)
+
+
+@bp.route("/recommendations", methods=["GET"])
+@login_required
+def get_recommendations(user_id: str):
+    result = db_provider.col_raw_book_datas.aggregate([
+        {
+            "$sample": {
+                "size": 10
+            }
+        }, {
+            "$match": {
+                "volumeInfo.maturityRating": {
+                    "$ne": "MATURE"
+                }
+            }
+        }, {
+            "$project": {
+                "_id": 1,
+                "id": 1,
+                "title": "$volumeInfo.title",
+                "subtitle": "$volumeInfo.subtitle",
+                "thumbnail": "$volumeInfo.imageLinks.thumbnail"
+            }
+        }
+    ])
+    
+    def _convert_id_to_str(doc):
+        doc["_id"] = str(doc["id"])
+        doc["id"] = str(doc["id"])
+
+        return doc
+
+    result = list(map(_convert_id_to_str, result))
+
+    return jsonify(result), 200
