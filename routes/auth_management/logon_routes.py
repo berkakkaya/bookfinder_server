2ca7230db25e3f504a5f09020847d98f1be5f46b
Berk Akkaya <berkakkaya000@gmail.com>
2024-12-04 22:50:02 +0300

    routes: add endpoints for login and register

---
+++ routes/auth_management/logon_routes.py
@@ -0,0 +1,76 @@
+from flask import Blueprint, request, jsonify
+from services.database import db_provider
+from utils.pw_ops import verify_password, hash_password
+from utils.token_management import generate_refresh_token, generate_access_token
+from pymongo.errors import DuplicateKeyError
+
+
+bp = Blueprint("logon_routes", __name__)
+
+@bp.route("/login", methods=["POST"])
+def post_login_route():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    # If email or password are missing, return an error
+    if not email or not password:
+        return jsonify({"error": "Missing email or password"}), 400
+
+    db_user = db_provider.col_users.find_one({"email": email})
+
+    # If user does not exist, return an unauthorized error
+    if not db_user:
+        return jsonify({"error": "Wrong email or password"}), 401
+
+    # Check if password is correct
+    is_pw_correct = verify_password(password, db_user["password"])
+
+    # If password is incorrect, return an unauthorized error
+    if not is_pw_correct:
+        return jsonify({"error": "Wrong email or password"}), 401
+
+    # Create a token for the user
+    refresh_token = generate_refresh_token(str(db_user["_id"]))
+    access_token = generate_access_token(str(db_user["_id"]))
+
+    return jsonify({
+        "refresh_token": refresh_token,
+        "access_token": access_token
+    }), 200
+
+
+@bp.route("/register", methods=["POST"])
+def post_register_route():
+    data = request.get_json()
+
+    name_surname = data.get("nameSurname")
+    email = data.get("email")
+    password = data.get("password")
+
+    # If user's name, email or password are missing, return an error
+    if not name_surname or not email or not password:
+        return jsonify({"error": "Missing name, email or password"}), 400
+
+    # Try to insert the user into the database
+    try:
+        result = db_provider.col_users.insert_one({
+            "nameSurname": name_surname,
+            "email": email,
+            "password": hash_password(password)
+        })
+    except DuplicateKeyError:
+        return jsonify({"error": "User with same email already exists"}), 409
+
+    # Get created user's id
+    user_id = str(result.inserted_id)
+
+    # Create a token for the user
+    refresh_token = generate_refresh_token(user_id)
+    access_token = generate_access_token(user_id)
+
+    return jsonify({
+        "refresh_token": refresh_token,
+        "access_token": access_token
+    }), 201
