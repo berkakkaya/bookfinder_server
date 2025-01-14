29a902ffdbc5a4e51ac8f980663571e939fb8082
Berk Akkaya <berkakkaya000@gmail.com>
2025-01-14 20:07:07 +0300

    feat: use new personalized feed generator

--- routes/recommendation_algorithm/book_recommendations.py
+++ routes/recommendation_algorithm/book_recommendations.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 
+from models.book_categories import BookCategory
 from services.database import db_provider
+from utils import pool_ops
 from utils.flask_auth import login_required
 
 bp = Blueprint("book_recommendations", __name__)
@@ -9,7 +11,6 @@ bp = Blueprint("book_recommendations", __name__)
 @bp.route("/recommendations", methods=["GET"])
 @login_required
 def get_recommendations(user_id: str):
-    # TODO: Overhaul this function to actually provide recommendations based on user's history
     param_category_filter = request.args.get("category")
 
     filters = {
@@ -22,29 +23,61 @@ def get_recommendations(user_id: str):
         }
     }
 
+    result = None
+
     if param_category_filter is not None:
+        if param_category_filter not in list(map(str, BookCategory)):
+            return jsonify({
+                "error": f"Invalid category '{param_category_filter}'"
+            }), 400
+
         filters["category"] = param_category_filter
 
-    result = db_provider.col_raw_book_datas.aggregate([
-        {
-            "$match": filters
-        },
-        {
-            "$sample": {
-                "size": 10
+        result = db_provider.col_raw_book_datas.aggregate([
+            {
+                "$match": filters
+            },
+            {
+                "$sample": {
+                    "size": 10
+                }
+            },
+            {
+                "$project": {
+                    "bookId": "$_id",
+                    "_id": 0,
+                    "title": "$volumeInfo.title",
+                    "authors": "$volumeInfo.authors",
+                    "description": "$volumeInfo.description",
+                    "thumbnail": "$volumeInfo.imageLinks.thumbnail"
+                }
             }
-        },
-        {
-            "$project": {
-                "bookId": "$_id",
-                "_id": 0,
-                "title": "$volumeInfo.title",
-                "authors": "$volumeInfo.authors",
-                "description": "$volumeInfo.description",
-                "thumbnail": "$volumeInfo.imageLinks.thumbnail"
+        ])
+    else:
+        result = pool_ops.get_personalized_recommendations(user_id)
+
+    if result is None:
+        # Get random books
+        result = db_provider.col_raw_book_datas.aggregate([
+            {
+                "$match": filters
+            },
+            {
+                "$sample": {
+                    "size": 10
+                }
+            },
+            {
+                "$project": {
+                    "bookId": "$_id",
+                    "_id": 0,
+                    "title": "$volumeInfo.title",
+                    "authors": "$volumeInfo.authors",
+                    "description": "$volumeInfo.description",
+                    "thumbnail": "$volumeInfo.imageLinks.thumbnail"
+                }
             }
-        }
-    ])
+        ])
 
     def _convert_id_to_str(doc):
         doc["bookId"] = str(doc["bookId"])
